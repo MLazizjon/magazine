1211a5ee479938a6332659da2f327335de8c80cb
MLazizjon <maxmudjonovlazizjon@gmail.com>
2026-07-16 00:36:14 +0500

    BISMILLAH TAVAKKaal"

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       />
 
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        {/* <Route path="/" element={<Navigate to="/login" />} /> */}
 
         {/* Ochiq sahifalar */}
         <Route path="/login" element={<Login />} />
